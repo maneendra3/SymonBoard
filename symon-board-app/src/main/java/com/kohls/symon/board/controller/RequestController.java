70860b981a6f77237ff04b1e3a8138900b3cc0c1
maneendra yarru <maneendra.yarru@kohls.com>
2018-06-25 13:24:01 +0530

    maven copy task

--- symon-board-app/src/main/java/com/kohls/symon/board/controller/RequestController.java
+++ symon-board-app/src/main/java/com/kohls/symon/board/controller/RequestController.java
@@ -2,7 +2,7 @@ package com.kohls.symon.board.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +10,7 @@ public class RequestController {
 
 	private static final Logger logger = LoggerFactory.getLogger(RequestController.class);
 
-	@PostMapping("/")
+	@GetMapping("/")
 	public String requestMethod() {
 		logger.info("Calling Request Method");
 		return "Welcome to Symon Board Replacement System.!";
