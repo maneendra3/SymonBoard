b98d0f4e556af6f37b14f163e1a50dbb32d46f2a
maneendra yarru <maneendra.yarru@kohls.com>
2018-06-25 16:12:33 +0530

    updations

--- symon-board-app/src/main/java/com/kohls/symon/board/controller/RequestController.java
+++ symon-board-app/src/main/java/com/kohls/symon/board/controller/RequestController.java
@@ -2,8 +2,8 @@ package com.kohls.symon.board.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class RequestController {
